c4ce69fd2cc9a167cdc0aeb998f5f6582b34905a
David Smith <dasmith2951@gmail.com>
2021-03-15 00:26:26 -0400

    incomplete MVP
    4/10 listeners. Out of time. I will restructure this for easier conceptualization by listing all variables at the top, & then proceed to implement the event listeners. *NTS* use Array.from() method to simplify this.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 // Your code goes here
+
+//1st & 2nd listeners. body - keyup/keydown styling
+
+let body = document.querySelector('body');
+
+body.addEventListener('keydown', (e) => {
+    e.target.style.backgroundColor = '#ff3333';
+});
+
+body.addEventListener('keyup', (e) => {
+    e.target.style.backgroundColor = '#ff5c33';
+});
+
+//3rd listener - double-click buttons styling
+
+let buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(element => {
+    element.addEventListener('dblclick', () => {
+        element.style.color = '#ffcc00';
+        element.style.border = '2px dashed purple';
+        element.style.width = '85px';
+        element.style.height = '25px';
+    })
+});
+
+//4th listener - right-click images styling
+
+let img = document.querySelectorAll('img');
+
+img.forEach(element => {
+    element.addEventListener('auxclick', () => {
+        element.style.border = '4mm ridge rgba(170, 50, 220, .6)';
+    })
+});
