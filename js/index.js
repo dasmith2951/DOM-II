b9fcb5fe4c5da87e3d21527e8550b41e0ae6e546
David Smith <dasmith2951@gmail.com>
2021-05-10 00:55:47 -0400

    9/10 listeners
    9/10 listeners, preventDefault(), & stopPropagation() added. MVP short one listener, I will add it in the morning.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 // Your code goes here
 
-//1st listener - mousedown
+let body = document.querySelector('body')
+
+//1st listener - key up
+
+body.addEventListener('keyup', e => {
+    body.style.backgroundColor = '#ff3333'
+})
+
+//2nd listener - key down
+
+body.addEventListener('keydown', e => {
+    body.style.backgroundColor = '#ff5c33'
+})
+
+//3rd listener - mousedown
 
 let funBusHeader = document.querySelector('h1')
 
@@ -8,20 +22,20 @@ funBusHeader.addEventListener('mousedown', e => {
     funBusHeader.textContent = "DOM Fun Bus Change"
 })
 
-//2nd listener - mouseup
+//4th listener - mouseup
 
 funBusHeader.addEventListener('mouseup', e => {
     funBusHeader.textContent = "Fun Bus"
 })
 
 
-//3rd listener - mouseover
+//5th listener - mouseover
 
 funBusHeader.addEventListener('mouseover', e => {
     funBusHeader.style.border = "5px dashed red"
 })
 
-//4th listener - mouseout
+//6th listener - mouseout
 
 funBusHeader.addEventListener('mouseout', e => {
     funBusHeader.style.border = "none"
@@ -38,11 +52,15 @@ nav.addEventListener('mouseout', e => {
     nav.style.border = "none"
 })
 
+nav.addEventListener('click', e => {
+    e.preventDefault()
+})
+
 //img section
 
 let img = document.querySelector('img')
 
-//5th listener - double click
+//7th listener - double click
 
 img.addEventListener('dblclick', e => {
     img.style.border = "5px dashed yellow"
@@ -50,14 +68,22 @@ img.addEventListener('dblclick', e => {
 
 let h2 = document.querySelector('h2')
 
-//6th listener - mouse enter
+//8th listener - mouse enter
 
 h2.addEventListener('mouseenter', e => {
     h2.style.border = "5px solid green";
 })
 
-//7th listener - mouse leave
+//9th listener - mouse leave
 
 h2.addEventListener('mouseleave', e => {
-    e.style.border ="none"
+    h2.style.border ="none"
 })
+
+let propagation = document.querySelector('#text')
+
+propagation[0].addEventListener('mousedown', e => {
+    propagation[0].style.border = "3px dashed white"
+    e.stopPropagation()
+})
+
